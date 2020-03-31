ce74567bfede64ce9b5bd42d18ab526463bb2b29
Danijel <kosijer84@gmail.com>
2020-04-01 00:09:32 +0200

    docs folder and files

---
+++ docs/precache-manifest.43513daf7f22e4b411a836c3ab00a608.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be1825690d2644e7e03019c8f28415fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a0499d1d201eac259a5",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a130fe570b2b52dc9222",
+    "url": "/static/js/2.87dc7396.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.87dc7396.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a0499d1d201eac259a5",
+    "url": "/static/js/main.6c81b069.chunk.js"
+  },
+  {
+    "revision": "de3c52ce1e4b55c5566b",
+    "url": "/static/js/runtime-main.30713609.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
